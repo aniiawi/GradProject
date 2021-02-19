986a0899db35f87b86e8c105186ee539ca332334
Anna Fadeeva <52171432+aniiawi@users.noreply.github.com>
2021-02-19 17:40:32 +0300

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -12,9 +12,9 @@ if ($user === false) {
   <head>
     <meta http-equiv="Content-type" content="text/html" charset="utf-8">
     <meta name="viewport" content="width=device-width, inital-scale=1">
-    <title>Рабочий стол - Веб-портал по лабораторным работам МАИ</title>
+    <title>Мои курсы - Веб-портал по лабораторным работам МАИ</title>
     <link rel="shortcut icon" href="https://dev.mai.ru/generic/images/favicon/1.6/favicon-2015.ico" type="image/x-icon">
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="/css/feedback.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <style>
         li {
@@ -31,7 +31,7 @@ if ($user === false) {
   		<nav class="menu-width-100" id="menu">
           <ul class="menu_list">
             <li class="homepage">
-        		<div type="submit" onclick="document.location='/index.php'" class="current"><img class="current-image" xmlns="http://www.w3.org/2000/svg" src="/img/home.svg"></img> <p>Рабочий стол </p></div></li>
+        		<div type="submit" onclick="document.location='/index.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/home2.svg"></img> Рабочий стол </div></li>
 
             <li class="theory">
         		<div type="submit" onclick="document.location='/courses.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/theory.svg"></img> Теория</div></li>
@@ -40,7 +40,7 @@ if ($user === false) {
         		<div type="submit" onclick="document.location='/tests.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/tests.svg"></img> Тесты</div></li>
 
             <li class="feedback">
-        		<div type="submit" onclick="document.location='/feedback.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/feedback.svg"></img> Обратная связь</div></li>
+        		<div type="submit" onclick="document.location='/feedback.php'" class="current"><img class="current-image" xmlns="http://www.w3.org/2000/svg" src="/img/feedback2.svg"></img> Обратная связь</div></li>
         	<?php
             if ($user['role_id'] < 3){
             ?>  <li class="add_llab">
@@ -64,7 +64,7 @@ if ($user === false) {
           <ul class="menu_list-mobile">
            
             <li class="homepage">
-            <div type="submit" onclick="document.location='/index.php'" class="current"><img class="current-image" xmlns="http://www.w3.org/2000/svg" src="/img/home.svg"></img> <p>Рабочий стол </p></div></li>
+            <div type="submit" onclick="document.location='/index.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/home.svg"></img> <p>Рабочий стол </p></div></li>
 
             <li class="theory">
             <div type="submit" onclick="document.location='/courses.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/theory.svg"></img> Теория</div></li>
@@ -73,7 +73,7 @@ if ($user === false) {
             <div type="submit" onclick="document.location='/tests.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/tests.svg"></img> Тесты</div></li>
 
             <li class="feedback">
-            <div type="submit" onclick="document.location='/feedback.php'" class="not_current"><img class="not-current-image" xmlns="http://www.w3.org/2000/svg" src="/img/feedback.svg"></img> Обратная связь</div></li>
+            <div type="submit" onclick="document.location='/feedback.php'" class="current"><img class="current-image" xmlns="http://www.w3.org/2000/svg" src="/img/feedback2.svg"></img> Обратная связь</div></li>
           <?php
             if ($user['role_id'] < 3){
             ?>  <li class="add_llab">
@@ -96,58 +96,39 @@ if ($user === false) {
       </div>
 
       <div class="working-area">
-        
-        
         <form autocomplete="off" class="search" action="/labs.php">
           <div class="autocomplete">
-            
           <input id="myInput" type="text" name="myCourses"  placeholder="Поиск курса...">
           <input type="image" id="myBtn" class="search-button" xmlns="http://www.w3.org/2000/svg" src="/img/search.svg"> </div>
-            <input type="hidden" name="course_id" id="courseid">
         </form>
-          <script  defer src="js/searchbar.js"></script>
-          <script>
-              <?php
-              if ($user['role_id'] == 1) {
-                  $myArr = dbtoarray($pdo);
-              }else{
-                  $myArr = getAssignedCourses($pdo, $user['user_id']);
-              }
-              $names_arr = array();
-              $ids_arr = array();
-              for($i=0; $i < count($myArr); $i++){
-                  $names_arr[] = $myArr[$i]["course_name"];
-                  $ids_arr[] = $myArr[$i]["course_id"];
-              };
-              ?>
-              var courses = [ <?php
-                  echo '"'.implode('","', $names_arr).'"'
-                  ?>];
-              var courses_id = [<?php
-                  echo '"'.implode('","', $ids_arr).'"'
-                  ?>];
-          </script>
-        <p class="desktop">Рабочий стол</p>
-        <p class="desktop-caption"> Мои лабораторные работы </p>
+        <script  defer src="js/searchbar.js"></script>
+        <script>
+            var courses = [<?php
+            $myArr = dbtoarray($pdo);
+            $names_arr = array();
+            for($i=0; $i < count($myArr); $i++){
+              $names_arr[] = $myArr[$i]["course_name"];
+            }
+            echo '"'.implode('","', $names_arr).'"'
+          ?>];
+        </script>
+        <p class="desktop">Обратная связь</p>
         <div class="cards">
-
-        <li class="card">
-          <p class="course-description"> Технический контроль и диагностика систем ЛА - Лабораторная работа №4 “Построение программы поиска неисправностей методом динамического программирования”</p>
-          <div type="button" onclick="document.location='/course.php'" class="course-goto"><p>Перейти</p></div>
-      </li>
-     <li class="card">
-          <p class="course-description"> Технический контроль и диагностика систем ЛА - Лабораторная работа №4 “Построение программы поиска неисправностей методом динамического программирования”</p>
-          <div type="button" onclick="document.location='/course.php'" class="course-goto"><p>Перейти</p></div>
-      </li>
-      <li class="card">
-          <p class="course-description"> Технический контроль и диагностика систем ЛА - Лабораторная работа №4 “Построение программы поиска неисправностей методом динамического программирования”</p>
-          <div type="button" onclick="document.location='/course.php'" class="course-goto"><p>Перейти</p></div>
-      </li>
-    </div>
+          <form name="form" action="mail.php" method="post" id="form_message">
+              <input type="text" class="user-input" placeholder="ФИО" name="fio-name" required>
+              <input type="text" class="user-input" placeholder="Группа" name="group-name" required>
+              <input type="text" class="user-input" placeholder="E-mail" name="email" required>
+                    
+            <input name="submit" type="submit" value="Отправить" class="send-button">
+  </form>
+        
+        </div>
+        
+       
     </div>
 
 
-      <div class="info-menu">
+    <div class="info-menu">
         <div class="info-contents">
         <div class="avatar"><img class="avatar-img" src="/img/prof_pic.png"></img></div>
         <p class="fio-name"> 
@@ -161,8 +142,7 @@ if ($user === false) {
           <ul class="course-list">
             <?php
             if ($user['role_id'] == 3){
-                for ($si=0; $si < count($myArr) && $si < 4; $si++){
-                $value = $myArr[$si];
+                foreach(getAssignedCourses($pdo, $user['user_id']) as $value){
              ?>
             <li class="choose-course">
               <div type="button" onclick="document.location='/course.php?course_id=<?=$value['course_id']; ?>'" class="course-button"><p class="button-text"><?=$value['course_name']; ?> </p><img class="course-image" xmlns="http://www.w3.org/2000/svg" src="/img/arrow.svg"></img> </div>
